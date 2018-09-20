4916ede0da7c793f7f0be1adfb409bc455c47afa
Stephan Rajaratnam <Stephan.Rajaratnam@mindgeek.com>
2018-09-20 08:20:25 -0400

    PATCH /todos/:id added

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const { ObjectID } = require('mongodb')
 const bodyParser = require('body-parser')
+const R = require('ramda')
+const _ = require('lodash');
 
 const { mongoose } = require('./db/mongoose')
 const { Todo } = require('./models/Todo')
@@ -66,6 +68,38 @@ app.delete('/todos/:id', (req, res) => {
 
   Todo
     .findOneAndDelete({ _id: id })
+    .then(todo => {
+      if (!todo) {
+        return res.status(404).send()
+      }
+      res.send({ todo })
+    })
+    .catch(err => res.status(400).send())
+})
+
+// PATCH /todos/:id
+app.patch('/todos/:id', (req, res) => {
+  const { id } = req.params
+  const body = R.pick(['text', 'completed'], req.body)
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send()
+  }
+
+  if (R.is(Boolean, body.completed) && body.completed) {
+    body.completedAt = new Date().getTime()
+  }
+  else {
+    body.competed = false
+    body.completedAt = null
+  }
+
+  Todo
+    .findOneAndUpdate(
+      { _id: id, },
+      { $set: body },
+      { new: true }
+    )
     .then(todo => {
       if (!todo) {
         return res.status(404).send()
